cede0581297718310b9a7a41dd9c2136d3c88e56
Arman <110356055+Winlogon-exe@users.noreply.github.com>
2023-07-19 16:34:52 +0300

    Update shared_ptr.cpp

--- shared_ptr.cpp
+++ shared_ptr.cpp
@@ -6,28 +6,30 @@ class shared_ptr
 {
 private:
 	T* ptr;
-	int* ref_count; // Счетчик ссылок
+	int* ref_count; // Reference count
 public:
-	//default
+	// Default constructor
 	explicit shared_ptr(T* pointer = nullptr) : ptr(pointer), ref_count(new int(1)) {}
 
-	//Конструктор копирования
-	shared_ptr(const shared_ptr& other):ptr(other.ptr),ref_count(other.ref_count){
+	// Copy constructor
+	shared_ptr(const shared_ptr& other) : ptr(other.ptr), ref_count(other.ref_count) {
 		++(*ref_count);
 	}
-	//Конструктор перемещения
-	shared_ptr(shared_ptr&& other) noexcept :ptr(other.ptr),ref_count(other.ref_count){
+
+	// Move constructor
+	shared_ptr(shared_ptr&& other) noexcept : ptr(other.ptr), ref_count(other.ref_count) {
 		other.ptr = nullptr;
 		other.ref_count = nullptr;
 	}
-	//Конструктор для массивов
-	shared_ptr(T* arrayPtr,size_t size):ptr(arrayPtr),ref_count(new int(1+size)){
+
+	// Constructor for arrays
+	shared_ptr(T* arrayPtr, size_t size) : ptr(arrayPtr), ref_count(new int(1 + size)) {
 		--(*ref_count);
 	}
 
-	shared_ptr& operator=(const shared_ptr& other){
-		if (!this == &other){
-			if (ptr && --(*ref_count) == 0){
+	shared_ptr& operator=(const shared_ptr& other) {
+		if (this != &other) {
+			if (ptr && --(*ref_count) == 0) {
 				delete ptr;
 				delete ref_count;
 			}
@@ -38,18 +40,18 @@ public:
 		return *this;
 	}
 
-	T&operator*(){
+	T& operator*() {
 		return *ptr;
 	}
 
-	T* operator ->(){
+	T* operator->() {
 		return ptr;
 	}
 
-	~shared_ptr(){
+	~shared_ptr() {
 		if (ptr) {
 			if (--(*ref_count) == 0) {
-				if (std::is_array<T>::value )
+				if (std::is_array<T>::value)
 					delete[] ptr;
 				else
 					delete ptr;
@@ -57,8 +59,8 @@ public:
 				delete ref_count;
 			}
 			else {
-				ptr == nullptr;
-				ref_count == nullptr;
+				ptr = nullptr;
+				ref_count = nullptr;
 			}
 		}
 	}
