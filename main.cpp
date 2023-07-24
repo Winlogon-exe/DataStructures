8732bf5ca84ffc8861cc9d6407042fbc9be413af
Arman <110356055+Winlogon-exe@users.noreply.github.com>
2023-07-24 18:44:43 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,7 @@
 #include <iostream>
 #include"shared_ptr.cpp"
-void foo(int x)
-{
-    int buf[10];
-    buf[x] = 0; 
-    if (x == 1000) {}   
-}
-
 int main()
 {
-	shared_ptr<int> a(new int[5]);
-    foo(1000);
+    shared_ptr<int> a(new int[5]);
     return 0;
-	
 }
